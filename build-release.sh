d166e7a302be620b334a4b7d98b8c5ee134793c9
aikhacode <aikhacomp@gmail.com>
2024-07-06 00:12:00 +0700

    coba

--- build-release.sh
+++ build-release.sh
@@ -6,18 +6,20 @@
 # it does not exit with 0, as we are interested in the final exit.
 # set -eo
 
+SLUG=${GITHUB_REPOSITORY#*/}
+
 generate_zip(){ 
     echo "Generating zip file..."
-    SLUG=${GITHUB_REPOSITORY#*/}
-    zip -r "${GITHUB_WORKSPACE}/${SLUG}.zip" dist/
+    
+    zip -r "${GITHUB_WORKSPACE}/${SLUG}.zip" "${SLUG}/"
     echo "zip-path=${GITHUB_WORKSPACE}/${SLUG}.zip" >> "${GITHUB_OUTPUT}"
     echo "${GITHUB_WORKSPACE}/${SLUG}.zip"
     echo "✓ Zip file generated!"
 }
 
 
-rsync -avr --exclude-from=".distignore" . dist --delete-after
-rsync -avr  --include="**build***" --include="*/" --exclude="*" wp-blocks/ dist/src/Blocks/
+rsync -avr --exclude-from=".distignore" . "${SLUG}" --delete-after
+rsync -avr  --include="**build***" --include="*/" --exclude="*" wp-blocks/ "${SLUG}/src/Blocks/"
 
 generate_zip
 
